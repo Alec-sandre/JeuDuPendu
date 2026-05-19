a2413e89586e5d6968adaaa31c904ede4eb38e42
Alec-sandre <alexandre.hallonet.1@ens.etsmtl.ca>
2026-05-19 10:07:08 -0400

    Début code, import random

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+Permet de jouer au jeu du pendu.
+Un seul import est requis pour faire fonctionner ce code : import random
+Deux choix seront possibles. Utiliser une liste de mots incluse ou la fournir.
+Pour simplifier la résolution, les majuscules/minuscules et accents sont assimilés à la même lettre.
+6 chances sont disponibles.
+Vous pouvez jouer et relancer, c'est parti !
+'''
+
+import random # permet d'utiliser la fonction random pour choisir un mot aléatoire
+
